9818aadb4ba40203e8fd1966f53fbfbba24ea9bc
Raphael Gault <gault_r@epita.fr>
2018-11-16 11:54:31 +0100

    types: Change register type from integer to mod type in order to be able to perform bitwise operations
    Signed-off-by: Raphael Gault <gault_r@epita.fr>

--- src/registers.ads
+++ src/registers.ads
@@ -4,9 +4,10 @@ package Registers is
   type Registers is record
     GeneralRegisters : GeneralRegs range 0 .. 15;
     I : Addr; -- Address pointer
-    VF : Unsigned_Int_8; -- Flag Register
-    DT : Unsigned_Int_8; -- Timer
+    --VF : Unsigned_Int_8; -- Flag Register
+    DT : Byte; -- Delay Timer
+    ST : Byte; -- Sound Timer
     PC : Integer_16;
-    SP : Unsigned_Integer_8;
+    SP : Byte;
   end record;
 end Registers;
--- src/types.ads
+++ src/types.ads
@@ -1,8 +1,7 @@
 package Types is
-  subtype Key is Integer range 0 .. 255;
+  subtype Byte is mod 256 with Size => 8;
   subtype Addr is Integer range 0 .. 4095;
   subtype Integer_16 is Integer range -32768 .. 32767;
-  subtype Unsigned_Int_8 is Integer range 0 .. 255;
-  type GeneralRegs is array (Integer) of Integer_16;
+  type GeneralRegs is array (Integer) of Byte;
   type RAM is array (Addr) of Integer_16;
 end Types;
