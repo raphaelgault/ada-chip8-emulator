829de5bc7d27f92db38f1cb44cee05b45d182d34
Tifanny Suyavong <tifanny.suyavong@epita.fr>
2018-12-04 19:08:23 +0100

    Instructions: add keyboard instruction handling
    Signed-off-by: Tifanny Suyavong <tifanny.suyavong@epita.fr>

--- src/class_e.adb
+++ src/class_e.adb
@@ -3,17 +3,27 @@ with Types; use Types;
 with Registers; use Registers;
 with Stack; use Stack;
 
+with Keyboard; use Keyboard;
+
 package body Class_E is
-  procedure SKP (x : Integer; vm : in out Registers.Registers)
+  procedure SKP (x : in Integer; vm : in out Registers.Registers)
   is
+     Keyboard : Keys := (others => false);
   begin
-    Put_Line ("SKP");
+     -- Put_Line ("SKP");
+     if Keyboard(X) = True then
+        Vm.Pc := Vm.Pc + 1;
+     end if;
   end SKP;
 
-  procedure SKNP (x : Integer; vm : in out Registers.Registers)
+  procedure SKNP (x : in Integer; vm : in out Registers.Registers)
   is
+     Keyboard : Keys := (others => false);
   begin
-    Put_Line ("SKP");
+     -- Put_Line ("SKNP");
+     if Keyboard(X) = False then
+        Vm.Pc := Vm.Pc + 1;
+     end if;
   end SKNP;
 
 end Class_E;
--- src/class_e.ads
+++ src/class_e.ads
@@ -3,6 +3,6 @@ with Registers; use Registers;
 with Stack; use Stack;
 
 package Class_E is
-  procedure SKP (x : Integer; vm : in out Registers.Registers);
-  procedure SKNP (x : Integer; vm : in out Registers.Registers);
+  procedure SKP (x : in Integer; vm : in out Registers.Registers);
+  procedure SKNP (x : in Integer; vm : in out Registers.Registers);
 end Class_E;
