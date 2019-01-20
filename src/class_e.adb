aedb493225f9442a335275bdf76b4d6aee7919ad
Raphael Gault <gault_r@epita.fr>
2019-01-20 14:19:08 +0100

    Keyboard: Fix SK(N)P instructions
    Signed-off-by: Raphael Gault <gault_r@epita.fr>

--- src/class_e.adb
+++ src/class_e.adb
@@ -8,18 +8,20 @@ with Keyboard; use Keyboard;
 package body Class_E is
   procedure SKP (x : in Integer; vm : in out Registers.Registers)
   is
+    K : Integer := Integer(VM.GeneralRegisters(X));
   begin
      -- Put_Line ("SKP");
-     if Vm.Pressed_Keys(X) = True then
+     if Vm.Pressed_Keys(K) = True then
         Vm.Pc := Vm.Pc + 2;
      end if;
   end SKP;
 
   procedure SKNP (x : in Integer; vm : in out Registers.Registers)
   is
+    K : Integer := Integer(VM.GeneralRegisters(X));
   begin
      -- Put_Line ("SKNP");
-     if Vm.Pressed_Keys(X) = False then
+     if Vm.Pressed_Keys(K) = False then
         Vm.Pc := Vm.Pc + 2;
      end if;
   end SKNP;
